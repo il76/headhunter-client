2029d63c547dddc489e3859d81f6c6fda9d2623f
Andrey Gurin <gurin.andreygv@yandex.ru>
2025-07-21 18:20:40 +0300

    Data класс с вакансией для внутреннего использования

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Vacancy.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.domain.models
+
+data class Vacancy(
+    val name: String,
+    val logoUrl: String,
+    val areaName: String,
+    val employerName: String,
+    val salaryCurrency: String,
+    val salaryFrom: Int?,
+    val salaryTo: Int?,
+    val experience: String,
+    val employment: String,
+    val description: String,
+    val keySkills: List<String>,
+)
