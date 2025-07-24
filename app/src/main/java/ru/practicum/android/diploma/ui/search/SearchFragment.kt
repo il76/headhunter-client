a308924e9d5e825d65483912b468f381cd67ebb1
il <igurylev@yandex.ru>
2025-07-24 16:43:43 +0300

    Добавил недостающие действия в навигационный граф. Добавил поле id в вакансию. Добавил онклик с дебаунсером и передачей id ваквнсии в фрагмент с информацией о вакансии.

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.util.debounce
 
 class SearchFragment : Fragment() {
 
@@ -14,6 +20,19 @@ class SearchFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: SearchViewModel by viewModel()
+
+    private val onVacancyClickDebounce by lazy {
+        debounce<Vacancy>(
+            CLICK_DEBOUNCE_DELAY,
+            viewLifecycleOwner.lifecycleScope,
+            false
+        ) { vacancy ->
+            findNavController().navigate(
+                R.id.action_searchFragment_to_vacancyFragment,
+                bundleOf("vacancyId" to vacancy.id)
+            )
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
@@ -23,6 +42,14 @@ class SearchFragment : Fragment() {
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        const val CLICK_DEBOUNCE_DELAY = 2000L
     }
 
 }
