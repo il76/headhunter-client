6c18807f7465a60135eb94a90323f55fae13a311
Andrey Gurin <gurin.andreygv@yandex.ru>
2025-07-26 07:10:56 +0300

    Добавлен служебный класс для функции Поделиться

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/sharing/ExternalNavigator.kt
@@ -0,0 +1,14 @@
+package ru.practicum.android.diploma.ui.sharing
+
+import android.content.Context
+import android.content.Intent
+
+class ExternalNavigator(private val context: Context) {
+    fun shareLink(link: String) {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK )
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, link)
+        context.startActivity(shareIntent)
+    }
+}
