b36949637466bfef679b52babcedd7e340b12f05
Marat Ikhsanov <ihsmen@gmail.com>
2025-07-26 14:43:21 +0300

    В репозиторий и интерактор добавить методы для получения данных

--- app/src/main/java/ru/practicum/android/diploma/data/network/HHApiService.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HHApiService.kt
@@ -1,10 +1,15 @@
 package ru.practicum.android.diploma.data.network
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.practicum.android.diploma.data.dto.VacancyDetailsResponse
 import ru.practicum.android.diploma.data.dto.VacancySearchResponse
 
 interface HHApiService {
     @GET("vacancies?")
     suspend fun getVacancies(@Query("text", encoded = false) text: String): VacancySearchResponse
+
+    @GET("vacancies/{vacancyId}")
+    suspend fun getVacancyDetails(@Path("vacancyId") vacancyId: String): VacancyDetailsResponse
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyInteractor.kt
@@ -2,7 +2,10 @@ package ru.practicum.android.diploma.domain.api
 
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.VacancyDetailsState
 
 interface VacancyInteractor {
     fun search(query: String, page: Int = 0): Flow<List<Vacancy>?>
+
+    fun getVacancyDetails(vacancyId: String): Flow<VacancyDetailsState>
 }
