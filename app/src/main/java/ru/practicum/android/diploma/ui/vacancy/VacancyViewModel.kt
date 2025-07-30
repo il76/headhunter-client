1b96789330044fb535e5ed30fe3e35821dc82f95
Marat Ikhsanov <ihsmen@gmail.com>
2025-07-30 17:11:30 +0300

    Добавил моковые данные для отображения вакансии

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
@@ -1,7 +1,40 @@
 package ru.practicum.android.diploma.ui.vacancy
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.domain.models.VacancyDetailsState
+import ru.practicum.android.diploma.domain.models.VacancyFull
 
 class VacancyViewModel : ViewModel() {
-    // заглушка
+    private val _screenState = MutableLiveData<VacancyDetailsState>(VacancyDetailsState.LoadingState)
+    val screenState: LiveData<VacancyDetailsState> = _screenState
+    private var id = -1
+
+    fun loadVacancy(vacancyId: Int): Unit {
+        id = vacancyId
+        _screenState.value = VacancyDetailsState.ContentState(mockVacancy)
+    }
+
+    val mockVacancy = VacancyFull(
+        id = 0,
+        name = "Android-разработчик",
+        company = "Еда",
+        currency = "₽",
+        salaryFrom = 100000,
+        salaryTo = null,
+        area = "Москва",
+        alternateUrl = "https://hh.ru/vacancy/8331228",
+        icon = "",
+        employment = "Полная занятость, Удаленная работа",
+        experience = "От 1 года до 3 лет",
+        schedule = "",
+        description = "<h3>Обязанности</h3><ul><li>Разрабатывать новую функциональность приложения</li><li>Помогать с интеграцией нашего SDK в другие приложения</li><li>Проектировать большие новые модули</li><li>Писать UI- и unit-тесты</li><li>Следить за работоспособностью\n",
+        contact = "",
+        email = "",
+        phone = "",
+        comment = "",
+        keySkills = "Знание классических алгоритмов и структуры данных, Программирование для Android больше одного года, Знание WebRTC",
+        address = ""
+    )
 }
