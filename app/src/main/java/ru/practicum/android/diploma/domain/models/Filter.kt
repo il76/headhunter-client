8d1d27328c45b24e6468b2a1c53fbeea8bd299e0
Marat Ikhsanov <ihsmen@gmail.com>
2025-08-04 15:51:06 +0300

    Исправить detekt

--- app/src/main/java/ru/practicum/android/diploma/domain/models/Filter.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Filter.kt
@@ -4,9 +4,4 @@ data class Filter(
     val salary: Int? = null,
     val industry: Industry? = null,
     val onlyWithSalary: Boolean? = null
-) {
-    fun hasAnyValue(): Boolean {
-        return salary != null || industry != null || onlyWithSalary != null
-    }
-}
-
+)
