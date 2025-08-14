90b5ba1ea4572197ada7b833ce18259fdac09151
Marat Ikhsanov <ihsmen@gmail.com>
2025-08-14 22:20:28 +0300

    Добавить обработку адреса в описании вакансии

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/Address.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.data.dto
+
+data class Address (
+    val building: String,
+    val city: String,
+    val lat: String,
+    val lng: String,
+    val street: String,
+    val description: String,
+)
